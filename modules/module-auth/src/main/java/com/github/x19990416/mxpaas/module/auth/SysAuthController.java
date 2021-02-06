1fa0e2213dda5d5a02380d043371bdb59a5d4e1c
macrossx <starseeker.limin@gmail.com>
2021-02-06 22:12:16 +0800

    ADD

--- modules/module-auth/src/main/java/com/github/x19990416/mxpaas/module/auth/SysAuthController.java
+++ modules/module-auth/src/main/java/com/github/x19990416/mxpaas/module/auth/SysAuthController.java
@@ -4,11 +4,18 @@
 package com.github.x19990416.mxpaas.module.auth;
 
 import com.github.x19990416.mxpaas.module.auth.domain.dto.UserPwdLogin;
+import com.wf.captcha.base.Captcha;
 import org.apache.shiro.authz.annotation.RequiresGuest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -19,7 +26,23 @@ public class SysAuthController {
     }
 
     @RequiresGuest
-    public void getCode(){
-
+    @GetMapping("/code")
+    public ResponseEntity<Object> getCode() {
+        // 获取运算的结果
+        Captcha captcha = loginProperties.getCaptcha();
+        String uuid = properties.getCodeKey() + IdUtil.simpleUUID();
+        //当验证码类型为 arithmetic时且长度 >= 2 时，captcha.text()的结果有几率为浮点型
+        String captchaValue = captcha.text();
+        if (captcha.getCharType() - 1 == LoginCodeEnum.arithmetic.ordinal() && captchaValue.contains(".")) {
+            captchaValue = captchaValue.split("\\.")[0];
+        }
+        // 保存
+        redisUtils.set(uuid, captchaValue, loginProperties.getLoginCode().getExpiration(), TimeUnit.MINUTES);
+        // 验证码信息
+        Map<String, Object> imgResult = new HashMap<String, Object>(2) {{
+            put("img", captcha.toBase64());
+            put("uuid", uuid);
+        }};
+        return ResponseEntity.ok(imgResult);
     }
 }
